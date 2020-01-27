91c41f97ebeb9f0d9e14b8aa0837bd2341e9edf1
arjunmara <arjun.marasini10@gmail.com>
2020-01-27 21:00:21 +0545

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Connect database
+connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Init middleware
+
+app.use(express.json({ extended: false }));
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve Static assets in prod
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, (req, res) => {
   console.log(`Server up on ${PORT}`);
 });
